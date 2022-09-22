5e71b096ddade2fc3f95131f90f397d135df125a
Victor Santos <61319771+aokami-aka@users.noreply.github.com>
2022-09-22 16:07:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -73,3 +73,45 @@ function ShowHidden(el) {
 	else
 		document.getElementById(el).style.display = 'none';
 }
+
+// ------------------ Slides Function -------------------------------------------------------------------------------------------------------------
+let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideId = ["mySlides1", "mySlides2","mySlides3","mySlides4","mySlides5","mySlides6", "mySlides7","mySlides8","mySlides9","mySlides10","mySlides11", "mySlides12","mySlides13","mySlides14","mySlides15","mySlides16", "mySlides17","mySlides18","mySlides19","mySlides20"] // .. multi slide class  name in array
+//initalization 
+showSlides(1, 0);  // zero  index
+showSlides(1, 1);  // first  
+showSlides(1, 2);  // second  
+showSlides(1, 3);  // third 
+showSlides(1, 4);   //four 
+showSlides(1, 5);  
+showSlides(1, 6);    
+showSlides(1, 7);   
+showSlides(1, 8);  
+showSlides(1, 9); 
+showSlides(1, 10);  
+showSlides(1, 11);    
+showSlides(1, 12);   
+showSlides(1, 13);  
+showSlides(1, 14);  
+showSlides(1, 15);  
+showSlides(1, 16);  
+showSlides(1, 17);    
+showSlides(1, 18);   
+showSlides(1, 19);  
+showSlides(1, 20);  
+//....
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
