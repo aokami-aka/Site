d1070f4d7ef9f2e84636b5836ce75be4b2624476
Victor Santos <61319771+aokami-aka@users.noreply.github.com>
2022-11-21 09:32:40 -0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -57,8 +57,8 @@ function dragElement(elmnt) {
 }
 
 // // ------------------ Slides Function -------------------------------------------------------------------------------------------------------------
-let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-var slideId = ["mySlides1", "mySlides2","mySlides3","mySlides4","mySlides5","mySlides6", "mySlides7","mySlides8","mySlides9","mySlides10","mySlides11", "mySlides12","mySlides13","mySlides14","mySlides15","mySlides16", "mySlides17","mySlides18","mySlides19","mySlides20"] // .. multi slide class  name in array
+let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideId = ["mySlides1", "mySlides2","mySlides3","mySlides4","mySlides5","mySlides6", "mySlides7","mySlides8","mySlides9","mySlides10","mySlides11", "mySlides12","mySlides13","mySlides14","mySlides15","mySlides16", "mySlides17","mySlides18","mySlides19","mySlides20","mySlides21","mySlides22","mySlides23","mySlides24","mySlides25","mySlides26","mySlides27","mySlides28","mySlides29","mySlides30"] // .. multi slide class  name in array
 //initalization 
 showSlides(1, 0);  // zero  index
 showSlides(1, 1);  // first  
@@ -81,6 +81,16 @@ showSlides(1, 17);
 showSlides(1, 18);   
 showSlides(1, 19);  
 showSlides(1, 20);  
+showSlides(1, 21); 
+showSlides(1, 22); 
+showSlides(1, 23); 
+showSlides(1, 24); 
+showSlides(1, 25); 
+showSlides(1, 26); 
+showSlides(1, 27); 
+showSlides(1, 28); 
+showSlides(1, 29); 
+showSlides(1, 30); 
 //....
 
 function plusSlides(n, no) {
