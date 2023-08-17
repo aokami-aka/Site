03a99aa7041758ae55d2db1f63a2ee03dcff2c05
Victor Santos <61319771+aokami-aka@users.noreply.github.com>
2023-08-16 21:29:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -229,10 +229,164 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
+  if (n > x.length) {slideIndex[no] = 1}
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
   x[slideIndex[no]-1].style.display = "block";  
 }
+
+// -------- New design ---------------------------
+
+function AnimeCard(title, titleEnglish, slideid, Thumb1, Thumb2, Thumb3, Thumb4, slidegroup, Synopsis, Genre, DirectorLink, Director, Director2Link, Director2,  DirectorWorks, StudioLink, Studio, Studio2Link, Studio2, StudioWorks, Time, Duration, Commentary, Anidb, Mal, Cr, Video1, Poster1, caption1, Video2, Poster2, caption2, Video3, Poster3, caption3) {
+            var animecard = document.querySelector('.animeCard');
+            var animeTitle = document.getElementById('anime-title');
+            var animetitleEnglish = document.getElementById('anime-title-english');
+            var animeThumb1 = document.getElementById('anime-cardThumb1');
+            var animeThumb2 = document.getElementById('anime-cardThumb2');
+            var animeThumb3 = document.getElementById('anime-cardThumb3');
+            var animeThumb4 = document.getElementById('anime-cardThumb4');
+			var prevslidebutton = document.querySelector('.prev');
+			var prevcurrentslideid = prevslidebutton.getAttribute('onclick');
+			var prevsetslideid = prevcurrentslideid.replace(/plusSlides\(-1, \d+\)/, "plusSlides(-1, " + slidegroup + ")");
+			var nextslidebutton = document.querySelector('.next');
+			var nextcurrentslideid = nextslidebutton.getAttribute('onclick');
+			var nextsetslideid = nextcurrentslideid.replace(/plusSlides\(1, \d+\)/, "plusSlides(1, " + slidegroup + ")");
+            let text = 1;
+            var text1 = document.getElementById('text1');
+            var text2 = document.getElementById('text2');
+            var text3 = document.getElementById('text3');
+            var text4 = document.getElementById('text4');
+            var animeSynopsis = document.getElementById('anime-synopsis');
+            var animeGenre = document.getElementById('anime-genre');
+            var animeDirectorLink = document.getElementById('anime-director-link');
+			var animeDirector2Link = document.getElementById('anime-director2-link');
+            var animeDirector = document.getElementById('anime-director');
+			var animeDirector2 = document.getElementById('anime-director2');
+            var animeDirectorWorks = document.getElementById('anime-director-works');
+            var animeStudioLink = document.getElementById('anime-studio-link');
+			var animeStudio2Link = document.getElementById('anime-studio2-link');
+            var animeStudio = document.getElementById('anime-studio');
+			var animeStudio2 = document.getElementById('anime-studio2');
+            var animeStudioWorks = document.getElementById('anime-studio-works');
+            var animeTime = document.getElementById('anime-time');
+            var animeDuration = document.getElementById('anime-duration');
+            var animeCommentary = document.getElementById('anime-commentary');
+            var animeAnidb = document.getElementById('anime-anidb');
+            var animeMal = document.getElementById('anime-mal');
+            var animeCr = document.getElementById('anime-cr');
+			var animeVideo1container = document.getElementById('anime-video1-container');
+            var animeVideo1 = document.getElementById('anime-video1');
+            var animeCaption1 = document.getElementById('anime-video1-caption');
+            var animeVideo2container = document.getElementById('anime-video2-container');
+            var animeVideo2 = document.getElementById('anime-video2');
+            var animeCaption2 = document.getElementById('anime-video2-caption');
+            var animeVideo3container = document.getElementById('anime-video3-container');
+            var animeVideo3 = document.getElementById('anime-video3');
+            var animeCaption3 = document.getElementById('anime-video3-caption');
+
+            if (animecard.style.display === 'none' || animeTitle.innerHTML !== title) {
+                animecard.style.display = 'block';
+                animeTitle.innerHTML = title;
+                animetitleEnglish.innerHTML = titleEnglish;
+				document.getElementById('thumb1-container').className = slideid+' fade';
+				document.getElementById('thumb2-container').className = slideid+' fade';
+				document.getElementById('thumb3-container').className = slideid+' fade';
+				document.getElementById('thumb4-container').className = slideid+' fade';
+                animeThumb1.src = Thumb1;
+                animeThumb2.src = Thumb2;
+                animeThumb3.src = Thumb3;
+                animeThumb4.src = Thumb4;
+				prevslidebutton.setAttribute("onclick", prevsetslideid);
+				nextslidebutton.setAttribute("onclick", nextsetslideid);
+                text1.innerHTML = '1 / ' + text
+                text2.innerHTML = '2 / ' + text
+                text3.innerHTML = '3 / ' + text
+                text4.innerHTML = '4 / ' + text
+                animeSynopsis.innerHTML = Synopsis;
+                animeSynopsis.innerHTML = Synopsis.replace(/\n/g, '<br>');
+                animeGenre.innerHTML = Genre;
+                animeDirectorLink.href = DirectorLink;
+				animeDirector2Link.href = Director2Link;
+                animeDirector.innerHTML = Director;
+				animeDirector2.innerHTML = Director2;
+                animeDirectorWorks.innerHTML = DirectorWorks;
+                animeStudioLink.href = StudioLink;
+				animeStudio2Link.href = Studio2Link;
+                animeStudio.src = Studio;
+				animeStudio2.src = Studio2;
+                animeStudioWorks.innerHTML = StudioWorks;
+                animeTime.innerHTML = Time;
+                animeDuration.innerHTML = Duration;
+                animeCommentary.innerHTML = Commentary;
+                animeAnidb.href = Anidb;
+                animeMal.href = Mal;
+                animeCr.href = Cr;
+                animeVideo1.src = Video1;
+                animeVideo1.poster = Poster1;
+                animeCaption1.innerHTML = caption1;
+                animeVideo2.src = Video2;
+                animeVideo2.poster = Poster2;
+                animeCaption2.innerHTML = caption2;
+                animeVideo3.src = Video3;
+                animeVideo3.poster = Poster3;
+                animeCaption3.innerHTML = caption3;
+            } else {
+                animecard.style.display = 'none';
+            }
+
+			if (Video1 == "false") {
+                animeVideo1container.style.display = 'none';
+            } else {
+                animeVideo1container.style.display = 'list-item';
+            }
+            if (Video2 == "false") {
+                animeVideo2container.style.display = 'none';
+            } else {
+                animeVideo2container.style.display = 'list-item';
+            }
+            if (Video3 == "false") {
+                animeVideo3container.style.display = 'none';
+            } else {
+                animeVideo3container.style.display = 'list-item';
+            }
+
+            if (Cr == "nolink") {
+                document.getElementById('cr-image').style.opacity = '60%';
+                document.getElementById('cr-image').style.cursor = 'default';
+				animeCr.href = '#anime-card'; animeCr.target = ""
+            } else {
+                document.getElementById('cr-image').style.opacity = '100%';
+                document.getElementById('cr-image').style.cursor = 'pointer';
+				animeCr.target = "_blank"
+            }
+            if (Cr == "false") {
+                document.getElementById('cr-image').style.display = 'none';
+            } else {
+                document.getElementById('cr-image').style.display = 'inherit';
+            }
+
+            if (Thumb2 == "false") {
+				animeThumb2.src = Thumb1, animeThumb3.src = Thumb1, animeThumb4.src = Thumb1;
+                document.getElementById('thumb1-container').className = slideid, document.getElementById('thumb2-container').className = slideid, document.getElementById('thumb3-container').className = slideid, document.getElementById('thumb4-container').className = slideid;
+				text1.innerHTML = '1 / ' + text, text2.innerHTML = '1 / ' + text, text3.innerHTML = '1 / ' + text, text4.innerHTML = '1 / ' + text;
+            } else if (Thumb3 == "false") {
+				animeThumb3.src = Thumb2, animeThumb4.src = Thumb2;
+                document.getElementById('thumb3-container').className = slideid, document.getElementById('thumb4-container').className = slideid;
+				text = 2;
+				text1.innerHTML = '1 / ' + text, text2.innerHTML = '2 / ' + text, text3.innerHTML = '2 / ' + text, text4.innerHTML = '2 / ' + text;
+            } else if (Thumb4 == "false") {
+				animeThumb4.src = Thumb3;
+                document.getElementById('thumb4-container').className = slideid;
+				text = 3;
+				text1.innerHTML = '1 / ' + text, text2.innerHTML = '2 / ' + text, text3.innerHTML = '3 / ' + text, text4.innerHTML = '3 / ' + text;
+            } else {
+                text = 4;
+                text1.innerHTML = '1 / ' + text, text2.innerHTML = '2 / ' + text, text3.innerHTML = '3 / ' + text, text4.innerHTML = '4 / ' + text;
+            }  
+
+			if (Studio2Link == '') {
+				document.getElementById('anime-studio2-link').style.display = 'none';
+			}
+        }
